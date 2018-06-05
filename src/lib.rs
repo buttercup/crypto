29bd6c6753c422f0a4df345367b846a00fd77cd5
Sallar Kaboli <sallar.kaboli@gmail.com>
2018-06-05 20:56:10 +0300

    Add library

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+extern crate rcw;
+
+use rcw::pbkdf2::{pbkdf2 as pbkdf2_core};
+use rcw::hmac::Hmac;
+use rcw::sha2::Sha256;
+
+pub fn pbkdf2(password: &str, salt: &str, iterations: u32, bits: usize) -> Vec<u8> {
+    let mut to_store = Vec::new();
+    let mut mac = Hmac::new(Sha256::new(), password.as_bytes());
+
+    to_store.resize(bits / 8, 0);
+    pbkdf2_core(&mut mac, salt.as_bytes(), iterations, &mut to_store);
+    to_store
+}
+
+#[test]
+fn it_works() {
+    let buf = pbkdf2("password", "salt", 500, 512);
+    assert_eq!(buf.len(), 64);
+    assert!(buf[0] > 0);
+    assert!(buf[63] > 0);
+}
